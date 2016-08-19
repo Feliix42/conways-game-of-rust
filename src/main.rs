a5709a1cfce8f543d959565a0d3c4d9a27461741
Felix Wittwer <dev@felixwittwer.de>
2016-08-19 10:33:28 +0200

    Add field and seeding

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,48 @@
 extern crate pancurses;
+extern crate rand;
+
+use rand::{thread_rng, Rng};
+
+#[derive(Debug)]
+enum CellState {
+    Alive,
+    Dead
+}
+
+impl Clone for CellState {
+    fn clone(&self) -> CellState {
+        match *self {
+            CellState::Alive => CellState::Alive,
+            CellState::Dead  => CellState::Dead
+        }
+    }
+}
+
+
+fn seed_field(size_y: u32, size_x: u32) -> Vec<Vec<CellState>> {
+    // initialize a random number generator
+    let mut rng = thread_rng();
+    let mut generator = rng.gen_iter::<u32>();
+
+    // generate a size_x * size_y field of cells
+    let mut field: Vec<Vec<CellState>> = vec![vec![CellState::Dead; size_x as usize]; size_y as usize];
+
+
+    for line in field.iter_mut() {
+        for cell in line.iter_mut() {
+            if generator.next().expect("You ran out of random numbers!") > ((u32::max_value() as f64) * 0.6) as u32 {
+                *cell = CellState::Alive;
+            }
+        }
+    }
+    field
+}
+
+
+#[allow(unused_variables)]
+fn draw(win: &pancurses::Window, field: &Vec<Vec<CellState>>) {
+}
+
 
 fn main() {
     // initialize the ncurses window
@@ -9,8 +53,12 @@ fn main() {
     win.mv(4, 14);
     win.printw("wow");
 
-
+    //initially seed the field and print it
+    let field = seed_field(20, 15);
+    draw(&win, &field);
     win.refresh();
+
+
     let _ = win.getch();
 
     // delete the window and close the ncurses session
